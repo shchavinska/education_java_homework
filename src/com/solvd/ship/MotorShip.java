5a717ae51de2d0ae24d9bd791420d957b3a88ea4
Anastasiia Shchavinska <vladimirovnaanastasia@gmail.com>
2020-12-29 00:29:37 +0200

    Added switch case and final variables

--- src/com/solvd/ship/MotorShip.java
+++ src/com/solvd/ship/MotorShip.java
@@ -2,15 +2,20 @@ package com.solvd.ship;
 
 public class MotorShip extends Ship{
     private String motorType;
+    public static final String MOTOR_TYPE_UNKNOWN = "Unknown";
+    public static final String MOTOR_TYPE_STEAM = "Steam";
+    public static final String MOTOR_TYPE_ELECTRIC = "Electric";
+    public static final String MOTOR_TYPE_FUEL = "Fuel";
+
 
     public MotorShip(){
         super();
-        motorType = "Unknown";
+        motorType = MOTOR_TYPE_UNKNOWN;
     }
 
     public MotorShip(String name){
         super(name);
-        motorType = "Unknown";
+        motorType = MOTOR_TYPE_UNKNOWN;
     }
 
     public void setMotorType(String motorType) {
@@ -25,5 +30,25 @@ public class MotorShip extends Ship{
     public void printFields() {
         super.printFields();
         System.out.println("motorType: " + motorType);
+    }
+
+    @Override
+    public void swim(){
+        System.out.print(getName() + ": ");
+        switch (this.motorType){
+            case MOTOR_TYPE_STEAM:
+                System.out.println("Chuh-chuh");
+                break;
+            case MOTOR_TYPE_ELECTRIC:
+                System.out.println("Bzzz-zzz");
+                break;
+            case MOTOR_TYPE_FUEL:
+                System.out.println("Drin-drin");
+                break;
+            case MOTOR_TYPE_UNKNOWN:
+            default:
+                System.out.println("**silence**");
+                break;
+        }
     }
 }
